b285026877061793c813461d49fef689d06d54ce
william davis <williamjsdavis@berkeley.edu>
2021-01-06 11:48:14 -0800

    Fixed animation example

--- examples/animation_example.jl
+++ examples/animation_example.jl
@@ -3,11 +3,11 @@ using ViscosityDrop
 ### Setup simulation
 filename = "example"
 dir = "."
-Ny, Nz = 32, 32
+Ny, Nz = 64, 64
 do_all(filename, dir, Ny=Ny, Nz=Nz, yLength=2, zLength=2,
                       ν=1e-1,
                       z_position=0.6, width=0.2, amplitude=-1e-1,
-                      Δt=0.002, stop_time=40.0, iteration_interval=1000,
+                      Δt=0.002, stop_time=120.0, iteration_interval=1000,
                       schedule_interval=0.5, output_fields = :all)
 
 ### Open simulation
@@ -24,16 +24,19 @@ iterations = parse.(Int, keys(file["timeseries/t"]))
 ### Make animations
 using Printf
 using Plots
+
+# Buoyancy
 anim1 = @animate for iter in iterations
     t = file["timeseries/t/$iter"]*7.4e-2
     b_snapshot = file["timeseries/b/$iter"][1, :, :]
 
     contourf(yb, zb, b_snapshot',color=:balance,
-             title="Vertical velocity, t=$(@sprintf("%.2f", t)) ms",
+             title="Buoyancy, t=$(@sprintf("%.2f", t)) ms",
              aspect_ratio=:equal)
 end
-gif(anim1, "vorticity.gif", fps = 15)
+gif(anim1, "buoyancy.gif", fps = 15)
 
+# Vorticity
 anim2 = @animate for iter in iterations
     t = file["timeseries/t/$iter"]*7.4e-2
     ω_snapshot = file["timeseries/ω/$iter"][1, 1:Ny, 1:Nz]
